3eaca5313f957a9645713fdbc2bcb949a208636a
AnwarMohamed <anwarelmakrahy@gmail.com>
2016-01-20 23:41:38 +0200

    updating slices pam module

--- pam_crcauth.py
+++ pam_crcauth.py
@@ -11,6 +11,7 @@ import base64
 import string
 import sys
 import grp, pwd 
+import traceback
 
 import ConfigParser
 
@@ -21,10 +22,12 @@ import MySQLdb
 dbengineClass=MySQLdb
 
 def pam_sm_authenticate(pamh, flags, argv):
-    resp=pamh.conversation(
-        pamh.Message(pamh.PAM_PROMPT_ECHO_OFF,"Password")
-    )
+    return pamh.PAM_SUCCESS
 
+def pam_sm_setcred(pamh, flags, argv):
+    return pamh.PAM_SUCCESS
+
+def pam_sm_acct_mgmt(pamh, flags, argv):
     try:
         user = pamh.get_user(None)
     except pamh.exception, e:
@@ -32,7 +35,10 @@ def pam_sm_authenticate(pamh, flags, argv):
     if user == None:
         return pamh.PAM_USER_UNKNOWN
 
-    groups = [g.gr_name for g in grp.getgrall() if 'crc-users' in g.gr_mem]
+    groups = [g.gr_name for g in grp.getgrall() if user in g.gr_mem]    
+    gid = pwd.getpwnam(user).pw_gid
+    groups.append(grp.getgrgid(gid).gr_name)
+
     if 'crc-users' not in groups:
         return pamh.PAM_SUCCESS
 
@@ -43,43 +49,41 @@ def pam_sm_authenticate(pamh, flags, argv):
             else:
                 None
 
-        connargs={
+        connargs = {
             'mysqldb': { 
             'host': safeConfigGet('database','host'),
             'user': safeConfigGet('database','user'),
             'passwd': safeConfigGet('database','password'),
-            'port': safeConfigGet('database','port'),
+            'port': int(safeConfigGet('database','port')),
             'db': safeConfigGet('database','db')
             }
-        }[dbengine]
+        }['mysqldb']
     
         for k in connargs.keys():
             if connargs[k] is None:
                 del connargs[k]
     
-        db=dbengineClass.connect( **connargs )
+        db = dbengineClass.connect( **connargs )
+
+        syslog.syslog ("checking user: {0}".format(user))
 
         cursor=db.cursor()
-        cursor.execute(config.get('query','select_statement'),(user))
+        cursor.execute("select count(*) from slices where user_name='{0}' and NOW() between start_time and end_time".format(user))
         count_raw=cursor.fetchone()[0]
 
         if int(count_raw) == 1:
-            syslog.syslog ("pam-crcauth.py match")
+            syslog.syslog ("pam-crcauth.py succeed")
             return pamh.PAM_SUCCESS
         else:
-            pamh.PAM_AUTH_ERR
-    except:
+            syslog.syslog ("pam-crcauth.py failed")
+            return pamh.PAM_ACCT_EXPIRED
+
+    except Exception, e:
         syslog.syslog ("pam-crcauth.py exception triggered")
+        syslog.syslog (traceback.format_exc())
         return pamh.PAM_SERVICE_ERR
 
     return pamh.PAM_SERVICE_ERR
-
-
-def pam_sm_setcred(pamh, flags, argv):
-    return pamh.PAM_SUCCESS
-
-def pam_sm_acct_mgmt(pamh, flags, argv):
-    return pamh.PAM_SUCCESS
 
 def pam_sm_open_session(pamh, flags, argv):
     return pamh.PAM_SUCCESS
